46448652716388beee9789b4ff02b12e1dbd479e
michelle-pittman <michelle.pittmann@gmail.com>
2022-03-08 14:39:33 +1100

    spaget

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,25 @@
-console.log('uwu')
+import { Client } from 'pg'; // SAME AS: const { Client } = require('pg');
+// also the same as 
+// import pg from 'pg'
+// const Client = pg.Client
+// destructuring
+
+const start = async () => {
+
+    const client = new Client({
+        database:"dinner_time",
+        host:"localhost",
+        user:"postgres",
+        password:"password",
+        port:5432,
+    })
+    await client.connect()
+    const res = await client.query('SELECT * FROM meals')
+    console.log(res.rows[0].name)
+    await client.end()
+
+}
+
+
+start();
+
