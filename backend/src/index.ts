b94847e6d05dd88a6255e0475870279c1b3a4660
michelle-pittman <michelle.pittmann@gmail.com>
2022-05-03 15:02:49 +1000

    add fastify with example route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,22 +9,49 @@ type Meal = {
     name: string,
 }
 
-const start = async () => {
+const starts = async () => {
 
     const client = new Client({
-        database:"dinner_time",
-        host:"localhost",
-        user:"postgres",
-        password:"password",
-        port:5432,
+        database: "dinner_time",
+        host: "localhost",
+        user: "postgres",
+        password: "password",
+        port: 5432,
     })
     await client.connect()
     const res = await client.query<Meal>('SELECT * FROM meals')
-    res.rows.forEach(meal => console.log(meal.id))
+    res.rows.forEach(meal => console.log(meal.name))
     await client.end()
 
 }
 
 
-start();
+starts();
 
+// Require the framework and instantiate it
+
+// ESM
+import Fastify from 'fastify'
+const fastify = Fastify({
+    logger: true
+})
+
+fastify.get('/meals', async (request, reply) => {
+    return {
+        meals: [{
+            name: "corn",
+            id: 2
+        }]
+    }
+})
+
+const start = async () => {
+    try {
+        await fastify.listen(3000)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
